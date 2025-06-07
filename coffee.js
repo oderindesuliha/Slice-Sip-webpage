4e437f4dc61109d0d96b9b50d627e11a7d06e8f9
Oderinde Suliha <oderindesuliha@gmail.com>
2025-06-07 18:45:49 +0100

    Slice and Sip webpage

--- coffee.js
+++ coffee.js
@@ -1,1 +1,46 @@
-console.log('Happy developing ✨')
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+
+
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-menu");
+});
+
+menuCloseButton.addEventListener("click", () => {
+    menuOpenButton.click();
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+});
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0:{
+        slidePerView: 1
+},
+768:{
+            slidePerView: 2
+},
+        1024:{
+            slidePerView: 3
+        }
+    }
+});
